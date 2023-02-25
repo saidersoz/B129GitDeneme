81e657c2705496fcdf936a5ad3e64d755c997fb0
saidErsoz <m.said.ersoz3443@gmail.com>
2023-02-25 18:44:47 +0300

    v2

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -2,6 +2,9 @@ public class Deneme {
 
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("Dünya");
     }
 
 }
+//Restore ==> Tamamen Working Space'i temizler.
+//Stash ==> Hafızaya alır.
