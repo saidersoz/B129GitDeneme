08820a0dae6fb750358d5734118e7e9d2bece23e
saidErsoz <m.said.ersoz3443@gmail.com>
2023-02-25 19:28:11 +0300

    v4

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -3,6 +3,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("Dünya");
+        System.out.println("B129");
     }
 
 }
